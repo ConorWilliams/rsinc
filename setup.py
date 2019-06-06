0df604d7c9b1cac4359680f20b3a78b707ce000c
ConorWilliams <conorwilliams@outlook.com>
2019-06-06 15:09:17 +0100

    script

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ConorWilliams/rsinc",
     packages=setuptools.find_packages(),
+    scripts=['bin/rsinc'],
     classifiers=[
         "Programming Language :: Python :: 3 ",
         "License :: OSI Approved :: MIT License",
