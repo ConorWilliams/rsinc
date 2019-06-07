964a90aed5c25c3488553c3460f7db8903a1df28
ConorWilliams <conorwilliams@outlook.com>
2019-06-07 23:43:13 +0100

    trace function

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rsinc",
-    version="1.0",
+    version="1.1",
     author="ConorWilliams",
     author_email="conorwilliams@outlook.com",
     description="A tiny, hackable, two-way cloud synchronisation client for Linux",
