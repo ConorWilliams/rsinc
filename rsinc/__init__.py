bb4b72d97d8990feaa81eba561e161c71edd96e9
ConorWilliams <conorwilliams@outlook.com>
2019-09-30 16:13:48 +0100

    versioning

--- rsinc/__init__.py
+++ rsinc/__init__.py
@@ -1,15 +1,2 @@
-<<<<<<< HEAD
-from .rsinc import sync
-from .rsinc import lsl
-from .rsinc import build_regexs
-from .rsinc import calc_states
-from .rsinc import make_dirs
-
-from .rsinc import Flat
-
-
-__version__ = "2.3-stable"
-=======
-__version__ = "2.4-DEV"
->>>>>>> master
+__version__ = "2.6-stable"
 __author__ = "ConorWilliams"
