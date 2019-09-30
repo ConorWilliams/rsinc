be90f1da583c3d7eca671fbef6dfea9143634e91
ConorWilliams <conorwilliams@outlook.com>
2019-09-30 16:08:13 +0100

    updated init

--- rsinc/__init__.py
+++ rsinc/__init__.py
@@ -1,10 +1,2 @@
-from .rsinc import sync
-from .rsinc import lsl
-from .rsinc import build_regexs
-from .rsinc import calc_states
-from .rsinc import make_dirs
-
-from .rsinc import Flat
-
 __version__ = "2.4-DEV"
 __author__ = "ConorWilliams"
